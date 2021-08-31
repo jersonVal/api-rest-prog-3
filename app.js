ba72113ba6c65b503f7fff85f8ef12d53bc12bad
jersonval96 <jersonDevelopment@gmail.com>
2021-08-31 14:15:27 -0500

    agregando app.js

---
+++ app.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import config from 'config';
+import bodyParser from 'body-parser';
+import getIp from './middleware/getIpAddress.js';
+import tkFn from './middleware/verifyToken.js';
+import db from './model/db-connection/mongodb.js'
+
+// Routes***********
+import studentRoutes from './routes/student.route.js'
+import teacherRoutes from './routes/teacher.route.js'
+import userRoutes from './routes/user.route.js'
+
+
+db();
+const app = express();
+const port = config.get('server-port')
+
+const jsonParser = bodyParser.json();
+const urlEncodeParser = bodyParser.urlencoded({
+    extended: true
+})
+
+app.use(jsonParser);
+app.use(urlEncodeParser);
+
+app.use('*', getIp);
+
+app.get('/', (req, res, next)=>{
+    res.send(`<h1>Welcome to academic rest api</h1>`)
+})
+
+userRoutes(app);
+
+//token middleware
+app.use(tkFn)
+
+studentRoutes(app);
+teacherRoutes(app);
+
+
+
+app.listen(port, ()=>{
+    console.log('server is running')
+})
